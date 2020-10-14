492ae868badba475bb5178f575579951b1eb36c1
dwinurhidayah <dwinurhidayah899@gmail.com>
2020-10-14 19:34:44 +0700

    third commit

---
+++ TUGAS 4/index.php
@@ -0,0 +1,184 @@
+<?php
+    $teks1 ="Hello, I'm Ida";
+    $teks2 ="This is my Curriculum Vitae.";
+    $teks3 ="Haloo! Kenalkan namaku Dwi Nurhidayah. Lahir tanggal 7 Juni 2000, sekarang aku berumur 20 tahun. Merasakan bangku kuliah di Informatika merupakan suatu keajaiban bagiku, bertemu dengan banyak teman baru dan banyak sekali hal-hal baru yang
+             aku pelajari, salah satunya adalah belajar Pemrograman Web, mata kuliah yang bisa manambah skill desain maupun coding.";
+    $teks4 ="Dwi Nurhidayah";
+    $teks5 ="Informatika’18 UPN VETERAN JAWA TIMUR";
+    $teks6 ="Abilities";
+    $taccounting ="Accounting";
+    $tmusic ="Music";
+    $torientation ="Orientation";
+    $tpositivity ="Positivity";
+    $tempathy ="Empathy";
+    $teks7 ="Academic Resume";
+    $teks8 ="SD
+    SDN KALANGKETI";
+    $teks81 ="SMP 
+    SMPN 4 MAGETAN";
+    $teks82 ="SMK
+    SMKN 1 MAGETAN";
+    $teks83 ="NOW
+    INFORMATIKA UPN V JATIM";
+    $teks9 ="Interest";
+    $teks10 ="Contacts";
+    $twatching ="Watching Korean Drama";
+    $ttravelling ="Travelling";
+    $tcyclyng ="Cyclyng";
+    $tlistening ="Listening Music";
+    $tsweets ="Sweets";
+    $tlove ="People I Love";
+    $tig ="@dnurhidayah_";
+    $ttwitter ="@dnurhidayah_";
+    $tline ="@idaa07";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>inicoba</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="a">
+        <img src="crop1.jpeg" alt="photo krop1" id="crop1" />
+        <img src="foto profil.png" alt="photo profil" id="fotoprofil" />
+        <div class="teks1">
+            <?php echo "$teks1";?>
+        </div>
+        <div class="teks2">
+            <?php echo "$teks2";?>
+        </div>
+        <div id="rectangle1"></div>
+        <div id="rectangle3"></div>
+        <div id="rectangle4"></div>
+        <div id="rectangle5"></div>
+        <div id="rectangle6"></div>
+        <div id="rectangle7"></div>
+    </div>
+
+    <div class="b">
+        <img src="crop2.jpeg" alt="photo krop2" id="crop2" />
+        <img src="foto profil.png" alt="photo profil 1" id="fotoprofil1" />
+        <div id="rectangle8">
+            <div class="teks3">
+            <p><?php echo "$teks3";?></p>
+            </div>
+        </div>
+        <div class="teks4">
+            <?php echo "$teks4";?>
+        </div>
+        <div class="teks5">
+            <?php echo "$teks5";?>
+        </div>
+        <div class="teks6">
+            <p><?php echo "$teks6";?></p>
+        </div>
+        <div id="rectangle9"></div>
+        <div id="rectangle10"></div>
+        <div id="rectangle11"></div>
+        <div id="rectangle12"></div>
+        <div id="rectangle13"></div>
+        <img src="calculating.png" alt="photo accounting" id="accounting" />
+        <img src="music.png" alt="photo music" id="music" />
+        <img src="orientation.png" alt="photo orientation" id="orientation" />
+        <img src="positivity.png" alt="photo positivity" id="positivity" />
+        <img src="empathy.png" alt="photo empathy" id="empathy" />
+        <div class="taccounting">
+            <?php echo "$taccounting";?>
+        </div>
+        <div class="tmusic">
+            <?php echo "$tmusic";?>
+        </div>
+        <div class="torientation">
+            <?php echo "$torientation";?>
+        </div>
+        <div class="tpositivity">
+            <?php echo "$tpositivity";?>
+        </div>
+        <div class="tempathy">
+            <?php echo "$tempathy";?>
+        </div>
+    </div>
+
+    <div class="c">
+        <img src="crop3.jpeg" alt="photo krop3" id="crop3" />
+        <div id="rectangle14"></div>
+        <div id="rectangle15"></div>
+        <div id="rectangle16"></div>
+        <div id="rectangle17"></div>
+        <div id="rectangle18"></div>
+        <div id="rectangle19"></div>
+        <div id="rectangle20"></div>
+        <div id="rectangle21"></div>
+        <div id="rectangle22"></div>
+        <div id="rectangle23"></div>
+        <div id="rectangle24"></div>
+        <div id="rectangle25"></div>
+        <div id="rectangle26"></div>
+        <div id="rectangle27"></div>
+        <div id="rectangle28"></div>
+        <div class="teks7">
+            <p><?php echo "$teks7";?></p>
+        </div>
+        <div class="teks8">
+            <?php echo "$teks8";?>
+        </div>
+        <div class="teks81">
+            <?php echo "$teks81";?>
+        </div>
+        <div class="teks82">
+            <?php echo "$teks82";?>
+        </div>
+        <div class="teks83">
+            <?php echo "$teks83";?>
+        </div>
+        <div class="teks9">
+           <p><?php echo "$teks9";?></p>
+        </div>
+        <div class="teks10">
+            <p><?php echo "$teks10";?></p>
+        </div>
+        <img src="tv-screen.png" alt="photo watching" id="watching" />
+        <img src="globe.png" alt="photo travelling" id="travelling" />
+        <img src="cycling.png" alt="photo cyclyng" id="cyclyng" />
+        <img src="headphones.png" alt="photo listening" id="listening" />
+        <img src="cupcake.png" alt="photo sweets" id="sweets" />
+        <img src="favourite.png" alt="photo love" id="love" />
+        <div class="twatching">
+            <?php echo "$twatching";?>
+        </div>
+        <div class="ttravelling">
+            <?php echo "$ttravelling";?>
+        </div>
+        <div class="tcyclyng">
+            <?php echo "$tcyclyng";?>
+        </div>
+        <div class="tlistening">
+            <?php echo "$tlistening";?>
+        </div>
+        <div class="tsweets">
+            <?php echo "$tsweets";?>
+        </div>
+        <div class="tlove">
+            <?php echo "$tlove";?>
+        </div>
+        <img src="instagram.png" alt="photo ig" id="ig" />
+        <img src="twitter.png" alt="photo twitter" id="twitter" />
+        <img src="line.png" alt="photo line" id="line" />
+        <div class="tig">
+            <?php echo "$tig";?>
+        </div>
+        <div class="ttwitter">
+            <?php echo "$ttwitter";?>
+        </div>
+        <div class="tline">
+            <?php echo "$tline";?>
+        </div>
+    </div>
+</body>
+
+</html>
